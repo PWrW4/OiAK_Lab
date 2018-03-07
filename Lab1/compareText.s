b0bb9ee4aeef824b19f3cc0c3214781338ea8474
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2018-03-07 18:39:07 +0100

    after wyklad

--- Lab1/compareText.s
+++ Lab1/compareText.s
@@ -1,10 +1,32 @@
 SYSEXIT = 1
 EXIT_SUCCESS = 0
+SYSWRITE = 4
+STDOUT = 1
+SYSREAD = 3
+STDIN = 0
+
+text: .ascii "asd"
+text_len = . - text
+
+.data
+
+buff: .ascii "   "
+buff_len = . - buff
 
 .global _start
 _start:
 
+mov $SYSREAD, %eax
+mov $STDIN, %ebx
+mov $buff, %ecx
+mov $buff_len, %edx
+int $0x80
 
+mov $SYSWRITE, %eax
+mov $STDOUT, %ebx
+mov $buff, %ecx
+mov $buff_len, %edx
+int $0x80
 
 mov $SYSEXIT, %eax
 mov $EXIT_SUCCESS, %ebx
