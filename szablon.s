b5ef0c61c14b040c1a8f1acd59439b2c5e272999
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2018-03-22 09:55:10 +0100

    after lab3

--- szablon.s
+++ szablon.s
@@ -1,3 +1,5 @@
+.code32
+
 SYSEXIT = 1;
 EXIT_SUCCESS = 0;
 STDOUT = 1
@@ -6,18 +8,19 @@ SYSREAD = 3
 STDIN = 0
 
 .text
-msg: .ascii "Hello! \n"
+msg: .ascii "Wojciech Wójcik - Hello! \n"
 msg_len = . - msg
-
-.data
-buf: .ascii "     "
-buf_len = . - buf
 
 .global _start
 _start:
 
-
+mov $SYSWRITE, %eax
+mov $STDOUT, %ebx
+mov $msg, %ecx
+mov $msg_len, %edx
+int $0x80
 
 mov $SYSEXIT, %eax
 mov $EXIT_SUCCESS, %ebx
 int $0x80
+
