27a8ab5ab2633f02fe55eff1f81479b594f2a7f8
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2018-04-10 12:38:21 +0200

    experimenting

--- Lab4/wektor.c
+++ Lab4/wektor.c
@@ -9,24 +9,25 @@ struct vec {
 
 struct vec Suma(struct vec a,struct vec b){
 asm( 
-    "movups "
-    "add %%ebx, %%eax"
-   : "=m" (a)
-   : "m" (b)
+    "movaps $0, %%xmm0"
+    "movaps $1, %%xmm1"
+    "addps %%xmm1, %%xmm0"
+    "movaps %%xmm0, $0"
+   : "=m" (&a)
+   : "m" (&b)
     );
     return a;
 }
 
 int main(){
-    struct vec v1;
-    struct vec v2;
-    v2.a = 4;
-    v2.b = 1;
-    v2.c = 1;
-    v2.d = 1;
+    struct  vec v1;
+    struct  vec v2;
     printf("Podaj 4 liczby : ");
     scanf("%d%d%d%d",&v1.a,&v1.b,&v1.c,&v1.d);
-    printf("\n Suma = %d \n", Suma(v1,v2).a);
+    printf("Podaj 4 liczby : ");
+    scanf("%d%d%d%d",&v2.a,&v2.b,&v2.c,&v2.d);
+    v1 = Suma(v1,v2);
+    printf("\n Suma = %d %d %d %d \n", v1.a, v1.b, v1.c, v1.d);
     //printf("\n Suma = %d \n", Suma(v1,v2).b);
     //printf("\n Suma = %d \n", Suma(v1,v2).c);
     //printf("\n Suma = %d \n", Suma(v1,v2).d);
