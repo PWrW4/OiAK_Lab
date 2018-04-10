d8154301f30af1a5ec2237ab71ad6aad41d5c304
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2018-04-10 17:48:49 +0200

    added -,*,/

--- Lab4/wektor.c
+++ Lab4/wektor.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct vec {
-    int a;
-    int b;
-    int c;
-    int d;
+    float a;
+    float b;
+    float c;
+    float d;
 };
 
-struct vec Suma(struct vec a,struct vec b){
+struct vec Sum(struct vec a,struct vec b){
 asm( 
     "movaps %0, %%xmm0\n\t"
     "movaps %1, %%xmm1\n\t"
@@ -19,18 +21,52 @@ asm(
     return a;
 }
 
+struct vec Sub(struct vec a,struct vec b){
+asm( 
+    "movaps %0, %%xmm0\n\t"
+    "movaps %1, %%xmm1\n\t"
+    "subps %%xmm1, %%xmm0\n\t"
+    "movaps %%xmm0, %0\n\t"
+   : "=m" (a)
+   : "m" (b)
+    );
+    return a;
+}
+
+struct vec Div(struct vec a,struct vec b){
+asm( 
+    "movaps %0, %%xmm0\n\t"
+    "movaps %1, %%xmm1\n\t"
+    "divps %%xmm1, %%xmm0\n\t"
+    "movaps %%xmm0, %0\n\t"
+   : "=m" (a)
+   : "m" (b)
+    );
+    return a;
+}
+
+struct vec Mul(struct vec a,struct vec b){
+asm( 
+    "movaps %0, %%xmm0\n\t"
+    "movaps %1, %%xmm1\n\t"
+    "mulps %%xmm1, %%xmm0\n\t"
+    "movaps %%xmm0, %0\n\t"
+   : "=m" (a)
+   : "m" (b)
+    );
+    return a;
+}
+
 int main(){
     struct  vec v1;
     struct  vec v2;
     printf("Podaj 4 liczby : ");
-    scanf("%d%d%d%d",&v1.a,&v1.b,&v1.c,&v1.d);
+    scanf("%f%f%f%f",&v1.a,&v1.b,&v1.c,&v1.d);
     printf("Podaj 4 liczby : ");
-    scanf("%d%d%d%d",&v2.a,&v2.b,&v2.c,&v2.d);
-    v1 = Suma(v1,v2);
-    printf("\n Suma = %d %d %d %d \n", v1.a, v1.b, v1.c, v1.d);
-    //printf("\n Suma = %d \n", Suma(v1,v2).b);
-    //printf("\n Suma = %d \n", Suma(v1,v2).c);
-    //printf("\n Suma = %d \n", Suma(v1,v2).d);
+    scanf("%f%f%f%f",&v2.a,&v2.b,&v2.c,&v2.d);
+    v1 = Sub(v1,v2);
+    printf("\n Suma = %f %f %f %f \n", v1.a, v1.b, v1.c, v1.d);
+
     
     return 0;
 }
